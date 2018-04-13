3e5d87c56ac78bcc08116dfde312f0886a7081d0
BOUVET\frode.vorraa <frode.vorraa@bouvet.no>
2018-04-13 14:01:54 +0200

    GridModule - Fixed incorrect comment (again)

--- lib/grid/grid.module.ts
+++ lib/grid/grid.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
   MatTableModule, MatSortModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatPaginatorModule, MatMenuModule
@@ -16,8 +15,6 @@ import { GridRowMenuComponent } from './row-menu/grid-row-menu.component';
   imports: [
     CommonModule,
     FormsModule,
-
-    //BrowserAnimationsModule,
 
     MatTableModule,
     MatSortModule,
