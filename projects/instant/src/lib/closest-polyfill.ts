16ae29dc3f4ac88a33c5d16d57706538cea875c0
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2018-08-13 14:05:03 +0200

    Added check for element

--- projects/instant/src/lib/closest-polyfill.ts
+++ projects/instant/src/lib/closest-polyfill.ts
@@ -1,19 +1,21 @@
-if (!Element.prototype.matches) {
-  Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
+if (typeof (Element) !== 'undefined') {
+  if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+  }
 
-if (!Element.prototype.closest) {
-  Element.prototype.closest = function (s) {
-    let el = this;
-    if (!document.documentElement.contains(el)) {
-      return null;
-    }
-    do {
-      if (el.matches(s)) {
-        return el;
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+      let el = this;
+      if (!document.documentElement.contains(el)) {
+        return null;
       }
-      el = el.parentElement || el.parentNode;
-    } while (el !== null && el.nodeType === 1);
-    return null;
-  };
+      do {
+        if (el.matches(s)) {
+          return el;
+        }
+        el = el.parentElement || el.parentNode;
+      } while (el !== null && el.nodeType === 1);
+      return null;
+    };
+  }
 }
